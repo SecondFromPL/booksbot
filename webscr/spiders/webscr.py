188e629870db1abb80885e6ea3ef1f34cb9d863a
SecondFromPL <145158784+SecondFromPL@users.noreply.github.com>
2025-11-20 23:41:15 +0100

    Update webscr.py

--- webscr/spiders/webscr.py
+++ webscr/spiders/webscr.py
@@ -1,5 +1,6 @@
 import scrapy
 from urllib.parse import urlparse, parse_qs, quote_plus
+import logging # DODANO
 
 class WebscrSpider(scrapy.Spider):
     name = 'webscr'
@@ -13,7 +14,6 @@ class WebscrSpider(scrapy.Spider):
     }
 
     def start_requests(self):
-        # Wymagane zapytania bez site:.pl i cudzysłowów
         queries = [
             'Sklep internetowy Shoper.pl -site:shoper.pl',
             'Oprogramowanie Shoper -site:shoper.pl',
@@ -22,24 +22,33 @@ class WebscrSpider(scrapy.Spider):
         
         for query in queries:
             encoded_query = quote_plus(query)
-            # Użycie wersji LITE z wymuszonym regionem PL (&kl=pl-pl)
             url = f'https://html.duckduckgo.com/lite/?q={encoded_query}&kl=pl-pl'
+            logging.info(f"START: Rozpoczynam scraping dla zapytania: {query}") # LOGOWANIE
             yield scrapy.Request(url=url, callback=self.parse_duckduckgo_results)
 
     def parse_duckduckgo_results(self, response):
         
-        # Parsowanie dla wersji LITE: wybieramy wszystkie linki zewnętrzne
+        # ZAPIS HTML: Zapisujemy kod HTML strony wyników do pliku
+        filename = f'ddg_lite_results_{response.url.split("q=")[-1].split("&")[0]}.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        logging.info(f"ZAPISANO HTML: Strona wyników DuckDuckGo zapisana do {filename}") # LOGOWANIE
+        
+        # Parsowanie dla wersji LITE
         result_links = response.xpath("//a[starts-with(@href, 'http')]/@href").getall()
         
+        logging.info(f"PARSOWANIE: Znaleziono {len(result_links)} potencjalnych linków na stronie wyników.") # LOGOWANIE
+
         for url in result_links:
-            # Filtrowanie linków wewnętrznych DDG
             if 'duckduckgo' not in url:
+                logging.info(f"LINK ZEWNĘTRZNY: Przechodzę do weryfikacji: {url}") # LOGOWANIE
                 yield scrapy.Request(url=url, callback=self.verify_shoper, meta={'handle_httpstatus_list': [403, 404, 500]})
 
         # Paginacja dla wersji LITE
         next_page = response.xpath("//a[contains(text(), 'Next') or contains(text(), 'Następne')]/@href").get()
         
         if next_page:
+            logging.info("PAGINACJA: Znaleziono link do następnej strony, kontynuuję.") # LOGOWANIE
             yield response.follow(next_page, callback=self.parse_duckduckgo_results)
             
     def verify_shoper(self, response):
@@ -63,9 +72,12 @@ class WebscrSpider(scrapy.Spider):
                     break
 
         if is_shoper:
+            logging.info(f"!!! ZNALEZIONO SKLEP: {response.url}") # LOGOWANIE
             yield {
                 'url': response.url,
                 'title': response.css('title::text').get(),
                 'generator': generator,
                 'detected': True
             }
+        
+        logging.info(f"WERYFIKACJA: Zakończono sprawdzanie: {response.url}") # LOGOWANIE
