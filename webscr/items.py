f07739f9c4a421273976b443b573704f50cefe00
SecondFromPL <145158784+SecondFromPL@users.noreply.github.com>
2025-11-20 22:49:02 +0100

    Update items.py

--- webscr/items.py
+++ webscr/items.py
@@ -1,14 +1,7 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
-
 class WebscrItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    url = scrapy.Field()
+    title = scrapy.Field()
+    generator = scrapy.Field()
+    detected = scrapy.Field()
