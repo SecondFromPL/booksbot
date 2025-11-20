39fa15c7979e6a3ec3e636524cf3754cd98cf121
SecondFromPL <145158784+SecondFromPL@users.noreply.github.com>
2025-11-20 22:54:07 +0100

    Update webscr.py

--- webscr/spiders/webscr.py
+++ webscr/spiders/webscr.py
@@ -1,5 +1,5 @@
 import scrapy
-from urllib.parse import urlparse, parse_qs
+from urllib.parse import urlparse, parse_qs, quote_plus
 
 class WebscrSpider(scrapy.Spider):
     name = 'webscr'
@@ -12,6 +12,11 @@ class WebscrSpider(scrapy.Spider):
         'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
     }
 
+    from urllib.parse import urlparse, parse_qs, quote_plus
+# upewnij się, że masz import quote_plus na górze pliku!
+
+# ...
+
     def start_requests(self):
         queries = [
             'site:.pl "Sklep internetowy Shoper"',
@@ -20,35 +25,29 @@ class WebscrSpider(scrapy.Spider):
         ]
         
         for query in queries:
-            url = f'https://www.google.com/search?q={query}'
-            yield scrapy.Request(
-                url=url, 
-                callback=self.parse_google_results, 
-                headers={
-                    'X-SmartProxy-User-Agent': 'desktop_chrome'
-                }
-            )
+            encoded_query = quote_plus(query)
+            # Używamy formatu html DDG
+            url = f'https://html.duckduckgo.com/html/?q={encoded_query}'
+            # Wracamy do prostego requestu bez zaawansowanych nagłówków JS
+            yield scrapy.Request(url=url, callback=self.parse_duckduckgo_results)
 
-    def parse_google_results(self, response):
-        result_links = response.xpath('//div[@class="g"]//a/@href').getall()
+
+def parse_duckduckgo_results(self, response):
         
-        if not result_links:
-            result_links = response.xpath('//div[contains(@class, "yuRUbf")]/a/@href').getall()
-            
-        for link in result_links:
-            url = link
-            
-            if '/url?q=' in link:
-                parsed = parse_qs(urlparse(link).query)
-                if 'q' in parsed:
-                    url = parsed['q'][0]
-            
-            if url.startswith('http') and 'google' not in url:
+        # Selekcja linków wyników (klasa result__url w DDG-H)
+        result_links = response.css('.result__url::attr(href)').getall()
+        
+        for url in result_links:
+            # DDG-H zwraca bezpośrednie URL, nie wymaga dekodowania /url?q=
+            if url.startswith('http') and 'duckduckgo' not in url:
                 yield scrapy.Request(url=url, callback=self.verify_shoper, meta={'handle_httpstatus_list': [403, 404, 500]})
 
-        next_page = response.css('a#pnnext::attr(href)').get()
+        # Selekcja linku do następnej strony 
+        next_page = response.xpath("//div[@id='content_bottom']//a[contains(text(), 'Next')]/@href").get()
+        
         if next_page:
-            yield response.follow(next_page, callback=self.parse_google_results)
+            # DuckDuckGo HTML pagination jest relatywna
+            yield response.follow(next_page, callback=self.parse_duckduckgo_results)
             
     def verify_shoper(self, response):
         is_shoper = False
